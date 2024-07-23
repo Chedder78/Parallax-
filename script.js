ed1b6b3691c12c7e8ef84c5c51d76224e5b76166
chad morrison <134284764+Chedder78@users.noreply.github.com>
2024-07-22 19:53:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('sliding-menu');
-    var overlay = document.getElementById('overlay');
-    if (menu.style.left === '0px') {
-        menu.style.left = '-250px';
-        overlay.style.display = 'none';
-    } else {
-        menu.style.left = '0px';
-        overlay.style.display = 'block';
-    }
-});
 
-document.getElementById('overlay').addEventListener('click', function() {
-    var menu = document.getElementById('sliding-menu');
-    menu.style.left = '-250px';
-    this.style.display = 'none';
-});
